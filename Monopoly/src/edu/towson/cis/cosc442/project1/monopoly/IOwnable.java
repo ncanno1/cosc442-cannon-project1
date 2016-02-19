296bf6652fb83fc039fc49ca889238303636dcf6
ncanno1 <ncanno1@students.towson.edu>
2016-02-19 04:27:10 -0500

    Generated Javadocs

--- Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/IOwnable.java
+++ Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/IOwnable.java
@@ -1,21 +1,66 @@
 package edu.towson.cis.cosc442.project1.monopoly;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface IOwnable.
+ */
 public interface IOwnable {
 
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
 	public abstract String getName();
 
+	/**
+	 * Gets the the owner.
+	 *
+	 * @return the the owner
+	 */
 	public abstract Player getTheOwner();
 
+	/**
+	 * Gets the price.
+	 *
+	 * @return the price
+	 */
 	public abstract int getPrice();
 
+	/**
+	 * Checks if is available.
+	 *
+	 * @return true, if is available
+	 */
 	public abstract boolean isAvailable();
 
+	/**
+	 * Play action.
+	 *
+	 * @param msg the msg
+	 * @return true, if successful
+	 */
 	public abstract boolean playAction(String msg);
 
+	/**
+	 * Sets the available.
+	 *
+	 * @param available the new available
+	 */
 	public abstract void setAvailable(boolean available);
 
+	/**
+	 * Sets the the owner.
+	 *
+	 * @param owner the new the owner
+	 */
 	public abstract void setTheOwner(Player owner);
 
+	/**
+	 * To string.
+	 *
+	 * @return the string
+	 */
 	public abstract String toString();
 
 }
